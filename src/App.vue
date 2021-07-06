9642688b08162bbc58c97a0c25c65408161b96aa
mantalbornoz <mantalbornoz@sodimac.cl>
2021-07-06 11:44:22 -0400

    se agrega peticion

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ export default{
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -39,5 +39,5 @@ export default{
 
 #nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
--- src/views/Character.vue
+++ src/views/Character.vue
@@ -15,7 +15,22 @@
 
 <script>
 export default {
-
+    
+methods:{
+    async obtenerPersonajes(){
+      try {
+          const url = `https://rickandmortyapi.com/api/character/${this.$route.params.id}`;
+            const data = await axios(url);
+            this.personajes = await data;
+            console.log(this.personajes.data.results);
+      } catch (error) {
+          console.error(error);
+      }
+    }
+  },
+  created(){
+  this.obtenerPersonajes()
+  }
 }
 </script>
 
